5c030437e5b0fb03b28e10f65d49d47b320f5164
Amy Westcott <amy.westcott@yahoo.com>
2019-06-27 22:01:13 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
 class BankAccount
-
+  attr_accessor :balance, :status
+  attr_reader :name
+  def initialize(name, balance = 1000, status = "open")
+    @name = name
+    @balance = balance 
+    @status = status
+  end 
+  
+  def deposit(deposit)
+    @balance += deposit
+  end 
+  
+  def display_balance
+    statement = "Your balance is $#{@balance}."
+  end 
+  
+  def valid? 
+    if @status == "open" && @balance > 0 
+      true 
+    else false 
+    end 
+  end 
+  
+  def close_account 
+    @status = "closed"
+  end 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,8 @@
 class Transfer
-  # your code here
+  attr_reader :transfer, :bank_account
+  attr_accessor :status, :sender
+  def initialize(sender, reciever, amou)
+    @transfer = transfer 
+    @sender = 
+  end 
 end
